1f8269f4171e0c181bb06b15ce6ead0b35fa6b28
hazg <hazg@mail.ru>
2015-07-18 16:57:03 +0300

    russian translation + fixes for 3-0-stable

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -1,10 +1,12 @@
 Spree::OptionValue.class_eval do
-
+  
   has_attached_file :image,
-    :styles        => { mini: '32x32>', normal: '128x128>' },
+    :styles        => { small: '32x32>', normal: '128x128>' },
     :default_style => SpreeVariantOptions::VariantConfig[:option_value_default_style],
     :url           => SpreeVariantOptions::VariantConfig[:option_value_url],
     :path          => SpreeVariantOptions::VariantConfig[:option_value_path]
+
+  validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
 
   def has_image?
     image_file_name && !image_file_name.empty?
