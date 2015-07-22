d475d3fa03a8433fd4d2c381b20cb0b7fc76ed9a
hazg <hazg@mail.ru>
2015-07-22 18:05:41 +0300

    before js cat

--- app/assets/javascripts/spree/frontend/spree_variant_options.js
+++ app/assets/javascripts/spree/frontend/spree_variant_options.js
@@ -2,7 +2,6 @@
 
 //= require spree/frontend/product_variant_options
 //= require spree/frontend/variant_options_script
-//= require spree/frontend/jquery.jcarousellite.min.js
 jQuery(function($){
   $('.variant-options-carousel').on('slid.bs.carousel', function () {
     $this = $(this);
